16a727f49202d1828166e55afd2bf64e1eb37e57
Błażej Zachariasz <blzc@demant.com>
2024-06-04 13:58:36 +0200

    initial commit with the app

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    
+    private void ImageSwitch_OnToggled(object? sender, ToggledEventArgs e)
+    {
+        MainThread.BeginInvokeOnMainThread(() => ChangeState(e.Value));
+    }
+
+    private void ChangeState(bool isVisible)
+    {
+        var thread = new Thread( () => VisualStateManager.GoToState(SampleImage, isVisible ? "Visible" : "Hidden"));
+        thread.Start();
+    }
+}
